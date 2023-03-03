ac49d949b99c294a7272ecd5f1c8e0b7a2007cd6
qishilong <663253062@qq.com>
2023-03-03 17:21:35 +0800

    二维数据结构
    二叉树
    二维数组

--- 13-二维数据结构/二维数组.js
+++ 13-二维数据结构/二维数组.js
@@ -1,5 +1,5 @@
 let averageTemp = [];
-// day 1
+
 averageTemp[0] = [];
 averageTemp[0][0] = 72;
 averageTemp[0][1] = 75;
@@ -7,7 +7,7 @@ averageTemp[0][2] = 79;
 averageTemp[0][3] = 79;
 averageTemp[0][4] = 81;
 averageTemp[0][5] = 81;
-// day 2
+
 averageTemp[1] = [];
 averageTemp[1][0] = 81;
 averageTemp[1][1] = 79;
