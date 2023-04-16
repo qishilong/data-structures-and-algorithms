2f70299c3543f2d3df9b66477e2d537846a90da9
qishilong <663253062@qq.com>
2023-04-16 13:43:06 +0800

    树的深度优先搜索
    树的深度优先搜索

---
+++ 27-树的深度优先搜索/index.js
@@ -0,0 +1,29 @@
+function Node(value) {
+    this.value = value;
+    this.childs = [];
+}
+
+var a = new Node("a");
+var b = new Node("b");
+var c = new Node("c");
+var d = new Node("d");
+var e = new Node("e");
+var f = new Node("f");
+
+a.childs.push(c);
+a.childs.push(f);
+a.childs.push(b);
+b.childs.push(d);
+b.childs.push(e);
+
+function deepSearch(root, target) {
+    if (root == null) return false;
+    if (root.value == target) return true;
+    var result = false;
+    for (var i = 0; i < root.childs.length; i++) {
+        result |= deepSearch(root.childs[i], target);
+    }
+    return result ? true : false;
+}
+
+console.log(deepSearch(a, "n"));
